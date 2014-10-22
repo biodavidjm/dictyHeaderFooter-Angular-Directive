c216f1e14d4a073fbedfdd4df6bcbe0e51e98673
David Jimenez-Morales <biodavidjm@gmail.com>
2014-10-22 14:37:12 -0500

    Moving the dynamicFooter content to the root
    This project was cloned from the frontpage-dictybase, with the
    intention of moving the angular directive of the footer to the root of
    this project

---
+++ doc.developersCorner.md
@@ -0,0 +1,9 @@
+Developers corner
+===================
+
+*Computational framework for using the dicty Header and Footer*
+
+---
+
+### Basic installations
+`node`, `npm`, `grunt`, `bower`, `karma`, `yo`
---
+++ test/spec/directives/directivesTest.js
@@ -0,0 +1,19 @@
+'use strict';
+
+describe('Directive: dictyFooter', function () {
+
+  // load the directive's module
+  beforeEach(module('dictyFooterApp'));
+
+  var element,scope;
+
+  beforeEach(inject(function ($rootScope) {
+    scope = $rootScope.$new();
+  }));
+
+  it('should make the element visible', inject(function ($compile) {
+    element = angular.element('<dicty-footer></dicty-footer>');
+    element = $compile(element)(scope);
+  }));
+
+});
