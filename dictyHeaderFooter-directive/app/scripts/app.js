a9556a5062464cba452efe72d83a9ba10dff6da2
David Jimenez-Morales <biodavidjm@gmail.com>
2014-11-14 16:09:09 -0600

    Creating a releasable version of dictyHF
    * This directory will host the version of the footer that will be
    release. All the other options in the folder will be just testing
    folders.

---
+++ dictyHeaderFooter-directive/app/scripts/app.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name backappApp
+ * @description
+ * # backappApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('testApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'dictyHeaderFooterApp'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
